371b043960e2296e53cbd38da50a54fbabc1abcb
Sakthivel10 <Sakthivel@shamlatech.com>
2025-01-22 16:56:02 +0530

    feat: hide activity log section

--- src/data/sideBarMenuItems.tsx
+++ src/data/sideBarMenuItems.tsx
@@ -11,7 +11,7 @@ export const sidebarMenuItems = [
   { label: "Rock Tournaments", icon: <StarIcon className="w-6 h-6" />, path: "/create-new-rock-tournament" },
   { label: "VIP Tournaments", icon: <AiOutlineCrown className="w-6 h-6" />, path: "/create-new-vip-tournament" },
   { label: "Tournament List", icon: <TableCellsIcon className="w-6 h-6" />, path: "/tournament-list" },
-  { label: "Activities", icon: <ClipboardIcon className="w-6 h-6" />, path: "/activities" },
+  // { label: "Activities", icon: <ClipboardIcon className="w-6 h-6" />, path: "/activities" },
   { label: "Logout", icon: <FiLogOut className="w-6 h-6" /> },
 ];
 
